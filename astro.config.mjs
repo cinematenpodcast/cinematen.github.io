f6324e4e361a90a96e36ee893220770634d0eeb9
cinematenpodcast <cinematenpodcast@gmail.com>
2024-08-14 13:16:05 +0200

    new version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,28 @@
-import { defineConfig } from 'astro/config';
-
-// In astro.config.mjs or where you configure routes
+// Import necessary packages
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import pagefind from "astro-pagefind";
 
-// https://astro.build/config
+// Export the combined configuration
 export default defineConfig({
 
-  // Other configurations...
-  routes: [{
-    // Define dynamic route for news pagination
-    pattern: '/nieuws/page/:page'
-    // Your specific setup here...
-  }, {
-    pattern: '/reviews&blogs/page/:page'
-  }],
-  integrations: [mdx()]
+  // Define routes for dynamic pagination
+  routes: [
+    {
+      // Define dynamic route for news pagination
+      pattern: '/nieuws/page/:page',
+      // Add any specific setup if needed
+    },
+    {
+      // Define dynamic route for reviews & blogs pagination
+      pattern: '/reviews&blogs/page/:page',
+      // Add any specific setup if needed
+    }
+  ],
+
+  // Integrations section
+  integrations: [
+    mdx(),
+    pagefind(),
+  ],
 });
